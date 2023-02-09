4109453c751c68885e2374cc27129f9dddb74de6
Kementary <Lautarivata@gmail.com>
2023-02-09 00:15:31 -0300

    inicio con el TP3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IcaroTP
 Trabajos Practicos Icaro
+
+inicio TP3 con node, a ver que onda
---
+++ TP3/TP3.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+//Tp3 Ej 1 "Repetir palabra del Usuario"
+
+let = palabra = readlineSync.question("ingrese su palabra")
+for(n=0;n<10;n++){
+    console.log(palabra);
+}
+
